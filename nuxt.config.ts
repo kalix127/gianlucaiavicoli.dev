9b11a9373fbea4b044da4dd77b012c2c7c2c2432
Gianluca Iavicoli <gianluca.iavicoli04@gmail.com>
2024-12-22 22:54:22 +0100

    chore: add tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,11 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
+
   future: {
     compatibilityVersion: 4,
   },
+
+  modules: ["@nuxtjs/tailwindcss"],
+  css: ["~/assets/css/main.css"],
 });

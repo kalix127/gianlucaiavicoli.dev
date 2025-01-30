b366928a1b32b4bdb1a3d22ac0861b8e56984e11
Gianluca Iavicoli <gianluca.iavicoli04@gmail.com>
2025-01-30 14:22:39 +0100

    feat(layout): add Footer

---
+++ app/components/layout/Footer.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const { footer } = useAppConfig();
+</script>
+
+<template>
+  <footer class="py-2 text-muted-foreground md:px-8 md:py-0">
+    <div class="flex items-center justify-between">
+      <div class="flex items-center gap-2">
+        {{ $t("footer.made_with") }}
+        &#10084;
+        {{ $t("footer.by") }}
+        <NuxtLink class="font-bold underline underline-offset-2" :to="footer.credits.to" target="_blank">
+          {{ footer.credits.title }}
+        </NuxtLink>
+      </div>
+      <p>
+        &copy; {{ new Date().getFullYear() }} Gianluca Iavicoli. {{ $t("footer.rights_reserved") }}.
+      </p>
+    </div>
+  </footer>
+</template>

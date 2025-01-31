3d507878d3045ffc1f87202f496d0c28cb5bbea2
Gianluca Iavicoli <gianluca.iavicoli04@gmail.com>
2025-01-31 20:57:34 +0100

    feat(layout): create default layout

---
+++ app/layouts/default.vue
@@ -0,0 +1,56 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <div class="wrapper">
+    <LayoutHeader />
+    <main>
+      <Background>
+        <slot />
+      </Background>
+    </main>
+    <LayoutFooter />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
+
+<style scoped>
+.wrapper {
+  display: grid;
+  min-height: 100svh;
+  grid-template-rows: 128px 1fr 84px;
+  grid-template-areas:
+    "header"
+    "main"
+    "footer";
+}
+@media (min-width: 319px) {
+  .wrapper {
+    grid-template-rows: 128px 1fr 64px;
+  }
+}
+
+@media (min-width: 768px) {
+  .wrapper {
+    grid-template-rows: 80px 1fr 64px;
+  }
+}
+
+header {
+  display: grid;
+  grid-area: header;
+}
+
+main {
+  display: grid;
+  grid-area: main;
+}
+
+footer {
+  display: grid;
+  grid-area: footer;
+}
+</style>

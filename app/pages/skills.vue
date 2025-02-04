d206a00b36c4f631ba5900e0e9cd1464ccf18e34
Gianluca Iavicoli <gianluca.iavicoli04@gmail.com>
2025-02-04 03:20:28 +0100

    feat(skills): add skills page

--- app/pages/skills.vue
+++ app/pages/skills.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+const { skills } = useAppConfig();
 const { t } = useI18n();
 
 useSeoMeta({
@@ -10,8 +11,26 @@ useSeoMeta({
 </script>
 
 <template>
-  <div class="grid h-full place-items-center">
-    Skills
+  <div class="grid h-full place-items-center space-y-8">
+    <div class="space-y-8">
+      <!-- Title -->
+      <h1 class="text-2xl font-bold sm:text-3xl md:text-4xl">
+        {{ $t("skills.title") }}
+      </h1>
+      <!-- Description -->
+      <p class="!mt-2 text-muted-foreground">
+        {{ $t("skills.description") }}
+      </p>
+
+      <!-- Languages -->
+      <SkillsCategory :title="$t('skills.programming_languages')" :items="skills.languages" />
+
+      <!-- Frameworks -->
+      <SkillsCategory :title="$t('skills.frameworks')" :items="skills.frameworks" />
+
+      <!-- Tools -->
+      <SkillsCategory :title="$t('skills.tools')" :items="skills.tools" />
+    </div>
   </div>
 </template>
 

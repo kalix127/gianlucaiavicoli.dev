498e86a1c2cbc4a493b7643d161958e04683298b
Gianluca Iavicoli <gianluca.iavicoli04@gmail.com>
2025-02-07 15:26:50 +0100

    feat(types): add Project interface and update Nuxt schema

--- index.d.ts
+++ index.d.ts
@@ -35,6 +35,10 @@ declare module "nuxt/schema" {
         url: string;
       }>;
     };
+    projects: {
+      personal: Array<Project>;
+      contributions: Array<Project>;
+    };
     footer: {
       credits: {
         title: string;
@@ -42,6 +46,18 @@ declare module "nuxt/schema" {
       };
     };
   }
+}
+
+export interface Project {
+  github?: string; // <github-username>/<github-repo>
+  title: string;
+  description: string;
+  link: string;
+  previewUrl?: string;
+  icons?: {
+    light?: string;
+    dark?: string;
+  };
 }
 
 export {};
